3c2866f2ac0f2c9d129c9e59281f0178c60df52a
Seba HA <hereniu.amaral.sebastian@gmail.com>
2022-11-07 13:05:18 -0300

    Subiendo Cuaderno de Ejercicios

---
+++ Curso-OpenBootcamp-C-Sharp/Curso-OpenBootcamp-C-Sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿//Ejercicio 1
+using System;
+
+namespace Curso_OpenBootcamp_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Humano quiero conocerlo...");
+            Console.WriteLine("Ingrese su nombre porfavor: ");
+            Console.WriteLine("==================================");
+            string nombre = Console.ReadLine();
+            string miNombre = "Sebastian Amaral, un placer conocerte!!";
+            Console.WriteLine("Hola " + nombre + ", el mio es " + miNombre);
+            Console.WriteLine("==================================");
+            Console.WriteLine("La hora actual es: " + DateTime.Now);
+        }
+    }
+}
